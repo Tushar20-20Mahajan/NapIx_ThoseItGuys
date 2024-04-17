2fcaeee167bc5d83bc2dfe41a5bc5db5f29232d0
Tushar20-20Mahajan <tusharmahajan080@gmail.com>
2024-04-17 16:34:27 +0530

    Initial Commit

---
+++ vehicleWise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vehicleWise
+//
+//  Created by student on 17/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
