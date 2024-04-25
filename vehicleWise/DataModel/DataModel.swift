b8284b43fb48f691407ead591ba0f5bd89170ed2
Tushar20-20Mahajan <tusharmahajan080@gmail.com>
2024-04-25 15:26:39 +0530

    Screens Completed and changes done

--- vehicleWise/DataModel/DataModel.swift
+++ vehicleWise/DataModel/DataModel.swift
@@ -15,11 +15,20 @@ struct Profile{
     var title: String
 }
 
-var profilesSectionOne : [Profile] = [
+struct Drivers{
+    var name: String
+    var mobile: String
+    var image: String
+}
+
+
+
+
+class DataModel{
+    var profilesSectionOne : [Profile] = [
         Profile(image: UIImage(systemName: "person.text.rectangle")!, title: "Personal Information"),
         Profile(image: UIImage(systemName: "figure.seated.seatbelt")!, title: "My Drivers"),
         Profile(image: UIImage(systemName: "questionmark.circle")!, title: "Help"),
         Profile(image: UIImage(systemName: "list.bullet.clipboard")!, title: "Privacy Policy")
     ]
-
-
+}
--- vehicleWise/LogisticInterface/ViewControllers/ProfileViewController.swift
+++ vehicleWise/LogisticInterface/ViewControllers/ProfileViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController , UITableViewDelegate, UITableViewDataSource {
+class ProfileViewController: UIViewController {
        
         
      
@@ -19,43 +19,43 @@ class ProfileViewController: UIViewController , UITableViewDelegate, UITableView
     override func viewDidLoad() {
         super.viewDidLoad()
             
-        tableViewOfAccountDisplayingInformation.dataSource = self
-        tableViewOfAccountDisplayingInformation.delegate = self
-        tableViewOfAccountDisplayingInformation.isScrollEnabled = false
-            
-            
-        tableViewOfAccountDisplayingInformation.register(UITableViewCell.self, forCellReuseIdentifier: "accountCell")
+//        tableViewOfAccountDisplayingInformation.dataSource = self
+//        tableViewOfAccountDisplayingInformation.delegate = self
+//        tableViewOfAccountDisplayingInformation.isScrollEnabled = false
+//            
+//            
+//        tableViewOfAccountDisplayingInformation.register(UITableViewCell.self, forCellReuseIdentifier: "accountCell")
             
     }
             
             // Profile Infomation profilesSectionOne
+//            
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//                return 1
+//    }
+//            
             
-    func numberOfSections(in tableView: UITableView) -> Int {
-                return 1
-    }
             
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//            return profilesSectionOne.count
+//    }
+//        
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//            let cell = tableView.dequeueReusableCell(withIdentifier: "accountCell", for: indexPath)
+//            let profile = profilesSectionOne[indexPath.row]
+//            cell.textLabel?.text = profile.title
+//            cell.imageView?.image = profile.image
+//            cell.accessoryType = .disclosureIndicator
+//            return cell
+//    }
             
-            
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return profilesSectionOne.count
-    }
-        
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "accountCell", for: indexPath)
-            let profile = profilesSectionOne[indexPath.row]
-            cell.textLabel?.text = profile.title
-            cell.imageView?.image = profile.image
-            cell.accessoryType = .disclosureIndicator
-            return cell
-    }
-            
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-                if let selectedIndexPath = tableView.indexPathForSelectedRow {
-                    tableView.deselectRow(at: selectedIndexPath, animated: true)
-                }
-                
-                tableView.selectRow(at: indexPath, animated: true, scrollPosition: .none)
-    }
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//                if let selectedIndexPath = tableView.indexPathForSelectedRow {
+//                    tableView.deselectRow(at: selectedIndexPath, animated: true)
+//                }
+//                
+//                tableView.selectRow(at: indexPath, animated: true, scrollPosition: .none)
+//    }
             
             
 }
