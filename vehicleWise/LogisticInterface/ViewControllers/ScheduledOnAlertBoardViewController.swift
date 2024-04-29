fe6dbd557d7e04fdc0452303825864f00691363c
Tushar20-20Mahajan <tusharmahajan080@gmail.com>
2024-04-29 14:18:10 +0530

    Scheduled Alerts on Alert Boaed completed

--- vehicleWise/LogisticInterface/ViewControllers/ScheduledOnAlertBoardViewController.swift
+++ vehicleWise/LogisticInterface/ViewControllers/ScheduledOnAlertBoardViewController.swift
@@ -7,23 +7,34 @@
 
 import UIKit
 
-class ScheduledOnAlertBoardViewController: UIViewController {
+class ScheduledOnAlertBoardViewController: UIViewController , UITableViewDataSource , UITableViewDelegate{
+    
+    
 
+    @IBOutlet weak var showScheduledList : UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        showScheduledList.dataSource = self
+        showScheduledList.delegate = self
+//        showScheduledList.register(vehicleNumberDisplayCell.self, forCellReuseIdentifier: "scheduledAlertBoard")
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataModel.getScheduledAlertOnAlertBoard().count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "scheduledAlertBoard") as? ScheduledAlertsTableViewCell{
+            let alerts = dataModel.getScheduledAlertOnAlertBoard()[indexPath.row]
+            
+            cell.updateViewOfScheduledAlertBoard(alerts: alerts)
+            
+            return cell
+        }
+        return ScheduledAlertsTableViewCell()
+    }
 
 }
